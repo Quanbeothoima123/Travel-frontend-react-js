6795cc3b66cd95e8593d1e6a3a6c385c3736ca8f
Quanbeothoima123 <phimanhh85@gmail.com>
2025-09-10 08:38:09 +0700

    Thay đổi lại cấu trúc file index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-import "./theme.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+// import "./index.css"
+// Thư viện CSS (import trước)
 import "bootstrap/dist/css/bootstrap.min.css";
 import "jquery/dist/jquery.min.js";
 import "popper.js/dist/popper.min.js";
 import "bootstrap/dist/js/bootstrap.min.js";
+
+// Theme CSS của bạn (import sau cùng để override)
+import "./theme.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
 reportWebVitals();
