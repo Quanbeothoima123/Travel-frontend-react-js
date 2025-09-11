7fa5df3a5dad5fa69d3a358ccf7735336087d399
Quanbeothoima123 <phimanhh85@gmail.com>
2025-09-11 15:32:25 +0700

    Viết lại LoadingModal cho chuyên nghiệp hơn

--- src/admin/components/common/LoadingModal/index.jsx
+++ src/admin/components/common/LoadingModal/index.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import * as FaIcons from "react-icons/fa";
 import "./LoadingModal.css";
 
-const LoadingModal = ({ open, message = "Đang xử lý..." }) => {
+const LoadingModal = ({ open, message = "Đang xử lý...", icon }) => {
   if (!open) return null;
+
+  const IconComponent = icon && FaIcons[icon] ? FaIcons[icon] : null;
 
   return (
     <div className="loading-overlay">
       <div className="loading-box">
-        <div className="spinner"></div>
+        <div className="spinner-wrapper">
+          {IconComponent && <IconComponent className="center-icon" size={24} />}
+          <div className="spinner-ring"></div>
+        </div>
         <p>{message}</p>
       </div>
     </div>
